bbf8e0e7f73250aae5f19b2b12e11200041677f0
eric-dishman-ctct <eric_dishman@trimble.com>
2025-05-11 07:07:29 -0500

    Updated pwsh profile

---
+++ powershell/profile.ps1
@@ -0,0 +1,108 @@
+$Env:EDITOR = 'nvim'
+$Env:KOMOREBI_CONFIG_HOME = "$Env:USERPROFILE\.config\komorebi"
+$Env:WHKD_CONFIG_HOME = "$Env:USERPROFILE\.config\whkd"
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Aliases
+Set-Alias -Name so      -Value Source-PowerShell-Profile
+Set-Alias -Name ls      -Value ShowDirectoryEza
+Set-Alias -Name lst     -Value ShowDirectoryEzaTree
+
+# Start Komorebi - Bar - WHKD
+function ks{komorebic start --whkd --bar}
+# Stop Komorebi - Bar - WHKD
+function kst{komorebic stop --whkd --bar}
+# Restart Komorebi - Bar - WHKD
+function rswm{
+    komorebic stop --whkd --bar
+    komorebic start --whkd --bar
+    }
+
+# Edit PowerShell Profile
+function epsp{nvim ($PROFILE.CurrentUserAllHosts)}
+# Edit Neovim Config
+function enc{
+  cd "$Env:USERPROFILE\AppData\Local\nvim"
+  nvim .
+}
+# Edit Git Config
+function egc{nvim "$Env:USERPROFILE\.gitconfig"}
+# Edit Komorebi Config
+function ekom{nvim "$Env:USERPROFILE\.config\komorebi\komorebi.json"}
+# Edit Komorebi Bar Config
+function ekbar{nvim "$Env:USERPROFILE\.config\komorebi\komorebi.bar.json"}
+# Edit WHKD Config
+function ewhkd{nvim "$Env:USERPROFILE\.config\whkd\whkdrc"}
+# Edit Wezterm Config
+function ewez{nvim "$Env:USERPROFILE\.config\wezterm\wezterm.lua"}
+# Edit PSReadline history
+function epsh{nvim (Get-PSReadLineOption | select -ExpandProperty HistorySavePath)}
+function gnc{fjira --project=GNC}
+function ShowDirectoryEza{eza --icons=always --color=always --git -al --group-directories-first}
+function ShowDirectoryEzaTree{eza --icons=always --color=always --git -al -T}
+function Source-PowerShell-Profile {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.SendKeys]::SendWait(". $")
+        [System.Windows.Forms.SendKeys]::SendWait("PROFILE.CurrentUserAllHosts")
+        [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+    }
+
+function yy {
+	$tmp = [System.IO.Path]::GetTempFileName()
+	yazi $args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+		Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+	}
+	Remove-Item -Path $tmp
+}
+
+function nvims()
+{
+  $items = "default", "MiniNvim"
+  $config = $items | fzf --prompt=" Neovim Config 󰄾 " --height=~50% --layout=reverse --border --exit-0
+
+  if ([string]::IsNullOrEmpty($config))
+  {
+    Write-Output "Nothing selected"
+    break
+  }
+ 
+  if ($config -eq "default")
+  {
+    $config = ""
+  }
+
+  $env:NVIM_APPNAME=$config
+  nvim $args
+}
+
+function nvimsf {nvims(fd -H -t f | fzf)}
+
+function m21b {C:\MATLAB\R2021b\bin\matlab.exe}  
+function m24b {C:\MATLAB\R2024b\bin\matlab.exe}  
+function nvimf {nvim(fd -H -t f | fzf)}
+function nvimd {nvim(fd -H -t d | fzf)}
+
+function zd {
+  $directory = fd -H -t d | fzf --prompt=" Search for path 󰄾 " --height=~50% --layout=reverse --border --exit-0
+
+  if ([string]::IsNullOrEmpty($directory))
+  {
+    Write-Output "Nothing selected"
+    break
+  }
+  
+  z $directory
+
+}
+
+# Obsidian workflow
+$Vault = "DishVault"
+function oo   {Set-Location -Path ("$Env:USERPROFILE" + "\" + $Vault)}
+function oib  {Set-Location -Path ("$Env:USERPROFILE" + "\" + $Vault + "\Inbox")}
+
+. "$HOME\scripting\custom_prompt.ps1"
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+#Set-PSDebug -Trace 0
